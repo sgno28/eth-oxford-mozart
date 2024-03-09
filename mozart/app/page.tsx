b306f5691113f4ecd217a25760f6b92e0bacf2ce
Steven Ngo <s.ngotuantu@gmail.com>
2024-03-09 01:22:22 +0000

    lightweight

--- mozart/app/page.tsx
+++ mozart/app/page.tsx
@@ -1,28 +1,22 @@
-import { Metadata } from "next"
-import Image from "next/image"
-import { PlusCircledIcon } from "@radix-ui/react-icons"
+import { Metadata } from "next";
+import Image from "next/image";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
 
-import { Button } from "@/components/ui/button"
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
-import { Separator } from "@/components/ui/separator"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import { AlbumArtwork } from "./components/album-artwork"
-import { Menu } from "./components/menu"
-import { PodcastEmptyPlaceholder } from "./components/podcast-empty-placeholder"
-import { Sidebar } from "./components/sidebar"
-import { listenNowAlbums, madeForYouAlbums } from "./data/albums"
-import { playlists } from "./data/playlists"
+import { AlbumArtwork } from "./components/album-artwork";
+import { PodcastEmptyPlaceholder } from "./components/podcast-empty-placeholder";
+import { Sidebar } from "./components/sidebar";
+import { listenNowAlbums, madeForYouAlbums } from "./data/albums";
+import { playlists } from "./data/playlists";
 
 export const metadata: Metadata = {
   title: "Music App",
   description: "Example music app using the components.",
-}
+};
 
 export default function MusicPage() {
   return (
@@ -44,7 +38,6 @@ export default function MusicPage() {
         />
       </div>
       <div className="hidden md:block">
-        <Menu />
         <div className="border-t">
           <div className="bg-background">
             <div className="grid lg:grid-cols-5">
@@ -52,23 +45,6 @@ export default function MusicPage() {
               <div className="col-span-3 lg:col-span-4 lg:border-l">
                 <div className="h-full px-4 py-6 lg:px-8">
                   <Tabs defaultValue="music" className="h-full space-y-6">
-                    <div className="space-between flex items-center">
-                      <TabsList>
-                        <TabsTrigger value="music" className="relative">
-                          Music
-                        </TabsTrigger>
-                        <TabsTrigger value="podcasts">Podcasts</TabsTrigger>
-                        <TabsTrigger value="live" disabled>
-                          Live
-                        </TabsTrigger>
-                      </TabsList>
-                      <div className="ml-auto mr-4">
-                        <Button>
-                          <PlusCircledIcon className="mr-2 h-4 w-4" />
-                          Add music
-                        </Button>
-                      </div>
-                    </div>
                     <TabsContent
                       value="music"
                       className="border-none p-0 outline-none"
@@ -86,6 +62,7 @@ export default function MusicPage() {
                       <Separator className="my-4" />
                       <div className="relative">
                         <ScrollArea>
+                          scroll area content
                           <div className="flex space-x-4 pb-4">
                             {listenNowAlbums.map((album) => (
                               <AlbumArtwork
@@ -112,6 +89,7 @@ export default function MusicPage() {
                       <Separator className="my-4" />
                       <div className="relative">
                         <ScrollArea>
+                          made for you content
                           <div className="flex space-x-4 pb-4">
                             {madeForYouAlbums.map((album) => (
                               <AlbumArtwork
@@ -153,5 +131,5 @@ export default function MusicPage() {
         </div>
       </div>
     </>
-  )
+  );
 }
