69fd2bb8e0b4a6af270e8d64d8d44b22d9353146
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2024-03-09 23:52:55 +0000

    [FEAT] Sidebar for creator/fan

--- mozart/src/ui/sidebar.tsx
+++ mozart/src/ui/sidebar.tsx
@@ -3,10 +3,29 @@ import { Button } from "@/ui/button";
 import { Separator } from "@/ui/separator";
 import { useMode } from "@/app/contexts/ModeContext";
 import { useRouter } from "next/navigation";
+import { RatIcon, BusIcon, BabyIcon, PlusIcon, CandlestickChart } from "lucide-react";
+import { LucideIcon } from "lucide-react/dist/lucide-react"
+
+type SidebarItem = {
+  name: string;
+  route: string;
+  icon: LucideIcon;
+};
 
 export function Sidebar() {
   const { mode, setMode } = useMode();
   const router = useRouter();
+
+  const fan_routes: SidebarItem[] = [
+    { name: "Discover", route: "/fan/discover", icon: RatIcon },
+    { name: "Hello", route: "/fan/my-creators", icon: BusIcon },
+    { name: "Mother", route: "/creator/my-bond", icon: BabyIcon },
+  ];
+
+  const creator_routes: SidebarItem[] = [
+    { name: "Add bond", route: "/creator/add-bond", icon: PlusIcon },
+    { name: "Bond Dashboard", route: "/creator/my-bond", icon: CandlestickChart },
+  ];
 
   const toggleMode = () => {
     const newMode = mode === "Fan" ? "Creator" : "Fan";
@@ -36,54 +55,55 @@ export function Sidebar() {
 
           <Separator className="horizontal my-2"></Separator>
           <div className="space-y-1 px-3">
-            <Button
-              variant="secondary"
-              className="w-full justify-start"
-              onClick={() => router.push("/fan/discover")} // Redirects to the Discover page
+            {mode === "Fan"
+              ? fan_routes.map((route: SidebarItem) => (
+                  <Button
+                    key={route.route}
+                    variant="secondary"
+                    className="w-full justify-start mb-1"
+                    onClick={() => router.push(route.route)}
+                  >
+                    <route.icon className="mr-2"/>
+                    {route.name}
+                  </Button>
+                ))
+              : creator_routes.map((route: SidebarItem) => (
+                  <Button
+                    key={route.name}
+                    variant="secondary"
+                    className="w-full justify-start mb-1"
+                    onClick={() => router.push(route.route)}
+                  >
+                    <route.icon className="mr-2"/>
+                    {route.name}
+                  </Button>
+                ))}
+          </div>
+        </div>
+      </div>
+      
+          <div className="px-3 py-6 flex justify-center items-center">
+            <p className="px-2">Switch Mode</p>
+            <div
+              onClick={toggleMode}
+              className="border rounded-md cursor-pointer overflow-hidden flex"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
+              <div
+                className={`flex-1 text-center py-2 px-2 ${
+                  mode === "Fan" ? "bg-black text-white" : "text-gray-800"
+                }`}
               >
-                <circle cx="12" cy="12" r="10" />
-                <polygon points="10 8 16 12 10 16 10 8" />
-              </svg>
-              Discover
-            </Button>
+                Fan
+              </div>
+              <div
+                className={`flex-1 text-center py-2 px-2 ${
+                  mode === "Creator" ? "bg-black text-white" : "text-gray-800"
+                }`}
+              >
+                Creator
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="px-3 py-6 flex justify-center items-center">
-        <p className="px-2">Switch Mode</p>
-        <div
-          onClick={toggleMode}
-          className="border rounded-md cursor-pointer overflow-hidden flex"
-        >
-          <div
-            className={`flex-1 text-center py-2 px-2 ${
-              mode === "Fan" ? "bg-black text-white" : "text-gray-800"
-            }`}
-          >
-            Fan
-          </div>
-          <div
-            className={`flex-1 text-center py-2 px-2 ${
-              mode === "Creator" ? "bg-black text-white" : "text-gray-800"
-            }`}
-          >
-            Creator
-          </div>
-        </div>
-      </div>
-    </div>
   );
 }
-
-const fan_routes: string[] = ["/fan/discover"];
-const creator_routes: string[] = [""];
