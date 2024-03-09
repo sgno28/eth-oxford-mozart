0eb3598486f1d0ce288441ed141459ee3d6acb69
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2024-03-09 12:33:35 +0000

    [CHORE] COmments

--- smart-contracts/contracts/Ticket.sol
+++ smart-contracts/contracts/Ticket.sol
@@ -4,12 +4,8 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./RevenueShare.sol"; // Assuming RevenueShare contract is in the same directory
+import "./RevenueShare.sol"; 
 
-/**
- * @title TicketFactory
- * @dev Contract for minting NFT tickets with a common IPFS URL for all tokens, where sale proceeds are routed to the musician's bond contract.
- */
 contract TicketFactory is ERC721, ReentrancyGuard, Ownable {
     uint256 private _nextTokenId;
     address public revenueShareAddress; // Address of the RevenueShare contract associated with the musician
@@ -23,11 +19,6 @@ contract TicketFactory is ERC721, ReentrancyGuard, Ownable {
         _commonIpfsUrl = commonIpfsUrl;
     }
 
-    /**
-     * @dev Mint a new ticket NFT and sends sale funds to the RevenueShare contract.
-     * @param to Recipient of the minted ticket.
-     * @param ticketPrice Price of the ticket being sold.
-     */
     function safeMint(address to, uint256 ticketPrice) public payable nonReentrant onlyOwner {
         require(msg.value == ticketPrice, "Incorrect payment amount");
         
@@ -39,27 +30,15 @@ contract TicketFactory is ERC721, ReentrancyGuard, Ownable {
         require(success, "Failed to forward funds to RevenueShare contract");
     }
 
-    /**
-     * @dev Override tokenURI to return the common IPFS URL for all token IDs.
-     * @param tokenId The ID of the token.
-     */
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         _requireOwned(tokenId);
         return _commonIpfsUrl;
     }
 
-    /**
-     * @dev Set or update the common IPFS URL for all tokens.
-     * @param commonIpfsUrl The new common IPFS URL.
-     */
     function setCommonIpfsUrl(string memory commonIpfsUrl) public onlyOwner {
         _commonIpfsUrl = commonIpfsUrl;
     }
 
-    /**
-     * @dev Set the RevenueShare contract address.
-     * @param _revenueShareAddress Address of the RevenueShare contract.
-     */
     function setRevenueShareAddress(address _revenueShareAddress) public onlyOwner {
         revenueShareAddress = _revenueShareAddress;
     }
