f4f4084686f87f3fd40de9c16ff94a72ab37c4cd
Steven Ngo <s.ngotuantu@gmail.com>
2024-03-10 08:20:47 +0000

    removed dummy data on fan page

--- mozart/src/app/fan/page.tsx
+++ mozart/src/app/fan/page.tsx
@@ -45,39 +45,39 @@ export default function Fan() {
   );
 }
 
-const dummy_creator_bond: Bond = {
-  contract_address: "0x1",
-  creator: "0x2",
-  principal_fee: 50,
-  revenue_share: 0.01,
-  expiry_date: 1712711617,
-  coupon_interval: 5,
-  supplyCap: 50,
-};
+// const dummy_creator_bond: Bond = {
+//   contract_address: "0x1",
+//   creator: "0x2",
+//   principal_fee: 50,
+//   revenue_share: 0.01,
+//   expiry_date: 1712711617,
+//   coupon_interval: 5,
+//   supplyCap: 50,
+// };
 
-const dummy_creator: Creator = {
-  spotifyId: "c1",
-  name: "test",
-  start_date: 1704852817,
-  followers: null,
-  web3_wallet: "0x1",
-  bond: dummy_creator_bond,
-  image: null,
-  ticketCollections: [],
-};
+// const dummy_creator: Creator = {
+//   spotifyId: "c1",
+//   name: "test",
+//   start_date: 1704852817,
+//   followers: null,
+//   web3_wallet: "0x1",
+//   bond: dummy_creator_bond,
+//   image: null,
+//   ticketCollections: [],
+// };
 
-const dummy_creators: Creator[] = [
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-  dummy_creator,
-];
+// const dummy_creators: Creator[] = [
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+//   dummy_creator,
+// ];
