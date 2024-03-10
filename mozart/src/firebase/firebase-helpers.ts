ca3d7aeba87e02ea5328845c33b84f4d6c4729c6
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2024-03-10 05:11:00 +0000

    [FEAT] Add ticket collection to creator object

--- mozart/src/firebase/firebase-helpers.ts
+++ mozart/src/firebase/firebase-helpers.ts
@@ -1,8 +1,53 @@
-import { getFirestore, doc, getDoc, updateDoc, collection, where, query, getDocs } from "firebase/firestore";
+import { getFirestore, doc, collection, where, query, getDocs, updateDoc, arrayUnion } from "firebase/firestore";
 import { app } from "./firebaseConfig";
 import { Bond } from "@/lib/interfaces";
+import { TicketCollection } from "@/lib/interfaces";
 
 const db = getFirestore(app);
+
+export async function addTicketCollectionToCreator(signerAddress: string, ticketCollection: TicketCollection) {
+    const creatorsRef = collection(db, "creators");
+    const creatorsQuery = query(creatorsRef, where("web3_wallet", "==", signerAddress));
+    const querySnapshot = await getDocs(creatorsQuery);
+
+    if (!querySnapshot.empty) {
+        const creatorDoc = querySnapshot.docs[0];
+        await updateDoc(creatorDoc.ref, {
+            ticketCollections: arrayUnion(ticketCollection)
+        });
+    } else {
+        console.error("Creator not found in Firestore");
+    }
+}
+
+export async function getTicketCollectionsByCreator(walletAddress: string) {
+    const creatorsRef = collection(db, "creators");
+    const q = query(creatorsRef, where("web3_wallet", "==", walletAddress));
+    const querySnapshot = await getDocs(q);
+  
+    if (!querySnapshot.empty) {
+      const docData = querySnapshot.docs[0].data();
+      return docData.ticketCollections as TicketCollection[];
+    } else {
+      console.error("Creator not found in Firestore");
+      return [];
+    }
+  }
+
+export async function getCreatorRevenueShareAddress(web3WalletAddress: string) {
+    // Reference to the creators collection
+    const creatorsRef = collection(db, "creators");
+    const creatorsQuery = query(creatorsRef, where("web3_wallet", "==", web3WalletAddress));
+    const querySnapshot = await getDocs(creatorsQuery);
+    
+    if (!querySnapshot.empty) {
+        const creatorDoc = querySnapshot.docs[0];
+        return creatorDoc.data().bond.contract_address;
+    } else {
+        console.error("Creator not found in Firestore");
+    }
+
+}
 
 export async function addBondToCreator(signerAddress: string, bond: Bond) {
     const creatorsRef = collection(db, "creators");
