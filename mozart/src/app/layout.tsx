18bfa57a17037c55a1677c68d0afd47107a96831
Steven Ngo <s.ngotuantu@gmail.com>
2024-03-10 14:09:24 +0000

    colour and layout

--- mozart/src/app/layout.tsx
+++ mozart/src/app/layout.tsx
@@ -20,18 +20,22 @@ export default function RootLayout({
           <ModeProvider>
             <div className="hidden md:block min-h-screen">
               <div className="border-t">
-                <div className="bg-background h-full">
+                <div className="bg-background bg-[#dafef1] h-full">
                   <div className="grid lg:grid-cols-5 h-full">
                     <Sidebar />
                     <div className="col-span-3 lg:col-span-4 lg:border-l overflow-auto">
-                      <header className="text-black p-3 text-center text-xl font-bold">
-                        Arya
-                      </header>
-                      <div className="justify-center items-center">
-                        <p className="italic px-4">Invest in your creators</p>
+                      <div className="border-t-2 border-black">
+                        <header className="text pt-8 text-center text-4xl font-bold">
+                          Arya
+                        </header>
+                        <div className="justify-center items-center">
+                          <p className="italic px-4 pb-2">
+                            Invest in your creators
+                          </p>
+                        </div>
                       </div>
-                      <Separator className="horizontal my-2"></Separator>
-                      {children}
+                      <Separator className="bg-[#211f1c]"></Separator>
+                      <div className="h-full bg-[#f1fffb]">{children}</div>
                     </div>
                   </div>
                 </div>
